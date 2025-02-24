c6b22a2608f80d34e3d96da02ad56b6d861e302d
BenjaminAlvarezDiaz <benja77744@gmail.com>
2025-02-24 01:05:39 -0300

    Problemas con esbuild
    ESTO NO ES UN MEME ES UN GRITO DE AYUDA

--- src/screens/NavbarGifts.jsx
+++ src/screens/NavbarGifts.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../styles/NavbarGifts.module.css";
 
 function NavbarGifts (){
--- src/screens/Puzz.jsx
+++ src/screens/Puzz.jsx
@@ -1,10 +1,41 @@
 import React from "react";
 import styles from "../styles/Puzz.module.css";
 
+import {JigsawPuzzle} from "react-jigsaw-puzzle/lib";
+import "react-jigsaw-puzzle/lib/jigsaw-puzzle.css";
+
+
 function Puzzle (){
+
+
+
     return (
         <div className={styles.main_container}>
-
+            <div className={styles.puzzle_container}>
+                <JigsawPuzzle 
+                    imageSrc="src/assets/1.png" 
+                    rows={4}
+                    columns={4}
+                    onSolved={() => alert("Solved!")}
+                />
+            </div>
+            <div className={styles.puzzle_container}>
+                <JigsawPuzzle 
+                    imageSrc="src/assets/2.jpg" 
+                    rows={4}
+                    columns={4}
+                    onSolved={() => alert("Solved!")}
+                />
+            </div>
+            <div className={styles.puzzle_container}>
+                <JigsawPuzzle 
+                    imageSrc="src/assets/3.jpg" 
+                    className={styles.puzzle}
+                    rows={4}
+                    columns={4}
+                    onSolved={() => alert("Solved!")}
+                />
+            </div>
         </div>
     );
 }
