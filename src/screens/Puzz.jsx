84ee402aa36317154969ba80a98fa563bc62236a
BenjaminAlvarezDiaz <benja77744@gmail.com>
2025-03-03 18:03:25 -0300

    Haciendo que se muestren distintas imagenes en el rompecabezas y mas
    Agregando un popup para mostrar la imagen completa para fines de guia, las imagenes son distintas entré si y siempre pueden cambiar, al recargar la pagina

--- src/screens/Puzz.jsx
+++ src/screens/Puzz.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState, useEffect } from "react";
 import styles from "../styles/Puzz.module.css";
+import { motion } from "framer-motion";
 
 import {JigsawPuzzle} from "react-jigsaw-puzzle/lib";
 import "react-jigsaw-puzzle/lib/jigsaw-puzzle.css";
@@ -14,22 +15,54 @@ import SlidingDialog from "../components/SlidingDialog/SlidingDialog";
 
 function Puzzle (){
     const swiperRef = useRef(null);
+    const puzzleRef = useRef(null);
+
+    const [images, setImages] = useState([]);
+    const [popupImage, setPopupImage] = useState(null);
+
     const [isTable, setIsTable] = useState(window.innerWidth < 1000);
     const [currentIndexSwiper, setCurrentIndexSwiper] = useState(0);
 
-    const images = [
-        "src/assets/1.png",
-        "src/assets/2.png",
-        "src/assets/3.png" 
-    ];
+    const openPopup = (image) => {
+        setPopupImage(image);
+    };
+
+    const closePopup = () => {
+        setPopupImage(null);
+    };
+
+    function getRandomNumbers() {
+        const numbers = new Set();
+        while (numbers.size < 3) {
+          numbers.add(Math.floor(Math.random() * 15) + 1);
+        }
+        const imagePaths = [...numbers].map(i => `src/assets/${i}.png`);
+        setImages(imagePaths);
+
+    };
+
+ 
+      
+    //console.log(getRandomNumbers()); // Ejemplo: [3, 12, 7]
 
     useEffect(() => {
         const resize = () => {
             setIsTable(window.innerWidth < 1000);
         };
+        const handleTouchMove = (e) => {
+            if (puzzleRef.current && puzzleRef.current.contains(e.target)) {
+              e.preventDefault(); // Evita el desplazamiento táctil
+            }
+        };
     
         window.addEventListener("resize", resize);
-        return () => window.removeEventListener("resize", resize);
+        document.addEventListener("touchmove", handleTouchMove, { passive: false });
+
+        return () => {
+            window.removeEventListener("resize", resize);
+            document.removeEventListener("touchmove", handleTouchMove);
+            getRandomNumbers();
+        };
       }, []);
 
     return (
@@ -45,7 +78,7 @@ function Puzzle (){
                     }}
                     spaceBetween={50}
                     slidesPerView={1}
-                    style={{ width: "320px", height: "360px"}}
+                    style={{ width: "320px", height: "360px", display: "flex", flexDirection: "column"}}
                     onSlideChange={(index) => {setCurrentIndexSwiper(index.activeIndex);}}
                 >
                 {images.map((image, index) => (
@@ -54,6 +87,7 @@ function Puzzle (){
                         style={{ display: "flex", justifyContent: "center" }}
                     >
                         <div 
+                            ref={puzzleRef}
                             className={styles.puzzle_container} 
                             onMouseEnter={() => swiperRef.current.swiper.allowTouchMove = false}
                             onMouseLeave={() => swiperRef.current.swiper.allowTouchMove = false}
@@ -68,6 +102,31 @@ function Puzzle (){
                     </SwiperSlide>
                 ))}
                 </Swiper>
+                <div 
+                    className={styles.tap_view_image} 
+                    onClick={() => openPopup(images[currentIndexSwiper])}
+                >
+                    Toca para ver la imagen
+                </div>
+                {popupImage && (
+                    <motion.div 
+                        className={styles.popup_overlay} 
+                        onClick={closePopup}
+                    >
+                    <motion.div className={styles.popup_content} onClick={(e) => e.stopPropagation()}>
+                        <motion.img 
+                            src={popupImage} 
+                            alt="Imagen completa"
+                            className={styles.popup_image} 
+                            initial={{ scale: 0 }} 
+                            animate={{ scale: 1 }} 
+                            exit={{ scale: 0 }} 
+                            transition={{ duration: 0.3 }}
+                        />
+                        <button className={styles.close_button} onClick={closePopup}>Cerrar</button>
+                    </motion.div>
+                    </motion.div>
+        )}
                 <div className={styles.custom_swiper_buttons}>
                     <div className={`${styles.custom_prev}`} 
                         style={{ opacity: currentIndexSwiper === 0 ? 0.2 : 1 }}><ion-icon name="chevron-back-outline"></ion-icon>
