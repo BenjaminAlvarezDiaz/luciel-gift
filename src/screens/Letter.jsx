7cce877e31d878749e4e9bb115f9c91e04dc88c6
BenjaminAlvarezDiaz <benja77744@gmail.com>
2025-03-12 02:57:56 -0300

    Termine todo el cuerpo de la pantalla de cartas
    Solo falta rellenar 2 cartas y mejorar, o no, las otras. Ademas de grabar un audio para la de su cumpleaños.

--- src/screens/Letter.jsx
+++ src/screens/Letter.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState, useEffect } from "react";
 import styles from "../styles/Letter.module.css";
+import { motion } from "framer-motion";
 
 function Letter (){
     const [isTable, setIsTable] = useState(window.innerWidth < 1000);
@@ -17,7 +18,49 @@ function Letter (){
     const lettersOfLove = [
         {
             condition: "Ábrela cuando busques inspiración",
-            content: "aaaaaaaaaa",
+            content: `Caí en un agujero 
+                    eternamente oscuro 
+                    Aunque puedo salir
+                    ¿Qué perderé después de ver la luz?
+
+                    Erase un árbol
+                    Y Erase alguien que se odiaba
+                    Erase un abrazo
+                    Y luego un estruendo
+                    Erase un alma
+                    Y luego nada
+                    Erase el mundo
+                    Erase yo
+
+                    No puedo hablar por mí
+                    No puedo sentir
+                    No puedo nadar
+                    No puedo ver que está bien
+                    Era un cadáver que podía hablar,
+                    dormía hasta irse con el viento
+                    Pensaba, pensaba y pensaba
+                    Pero nunca se levantaba de su tumba
+                    No quería caminar, no quería pudrirse
+                    Así que cerraba sus ojos
+                    Y se imaginaba con vida
+
+                    Le dije que tuve un sueño,
+                    que el cielo me hablaba y no le pude entender
+                    así que desperté
+                    Le que dije que para mí el sol es un punto
+                    que desaparece apenas abro los ojos
+                    Le dije que duermo sobre cadenas y cables
+                    que se clavan en mi piel
+                    Le dije que persigo las manecillas de un reloj furioso
+                    que desearía estuviera roto
+
+                    Me pregunta si el camino largo o corto
+                    Me pregunto si importa
+                    Ambos llevan al mismo lugar
+                    Quiero dejar de caminar correr en su lugar
+                    Pero me detengo en el ultimo paso
+                    Al borde Un paso del final
+                    Tengo miedo`,
         },
         {
             condition: "Ábrela cuando sea tu cumpleaños",
@@ -25,15 +68,55 @@ function Letter (){
         },
         {
             condition: "Ábrela cuando estés muy aburrida",
-            content: "aaaaaaaaa",
+            content: `Hoy regalé una flor, 
+                    perdida en mis recuerdos 
+
+                    No encuentro salida, pues no existe 
+                    Cien vacíos devoran esta mente 
+                    Donde sólo demonios habitan 
+
+                    No encuentro una razón, pues la perdí hace tiempo 
+                    La estática es un sótano oscuro 
+                    Donde sólo las ratas anidan 
+
+                    ¿Hace cuánto tus manos no sienten calidez? 
+                    ¿Hace cuánto no encuentras perdón? 
+                    ¿Puedes si quiera sentir el latir en tu pecho? 
+
+                    Pecador, arquitecto en su laberinto 
+                    Arquitecto de desgracias 
+                    Propias y ajenas, viejas y nuevas. 
+                    Engañado por su corazón egoísta 
+                    Una petición, miedo y gritos 
+                    Lagrimas, piedad y suplicas 
+                    Sufre, ya que no las reconoce 
+
+                    Hoy he perdido una flor 
+                    La única en mis recuerdos
+                    la última`,
         },
         {
-            condition: "Ábrela cuando desaparezcas por 3 meses",
-            content: "aaaaaaaa",
+            condition: "Ábrela cuando desaparezcas por varios meses",
+            content: `Vaya vaya... 
+                    Pero miren quien volvío despues de varios meses
+                    ¿Cuanto tiempo fue está vez? ¿Mas de 3 meses?
+                    Si es así, felicidades, rompiste tu record
+                    ¿Se siente bien aparecer de nuevo de forma misteriosa
+                    y entre las sombras?
+                    Como sea, date prisa y ve a hablarme que seguro te extrañe`,
         },
         {
-            condition: "Ábrela cuando sientas que el mundo es aburrido",
-            content: "aaaaaaaaaa",
+            condition: "Ábrela cuando sientas que nada tiene sentido",
+            content: `Escribe
+                    esa es la clave
+                    la capacidad de poder expresarnos a travez de palabras
+                    es sorprendente ¿Verdad? Y tú lo haces muy bien, 
+                    se que te funcionara, ademas
+                    cuando me siento igual utilizo la escritura para desahogarme,
+                    si bien soy mediocre
+                    ayuda bastante a poner en blanco y negro lo que me atormenta,
+                    si no funciona cuentame lo que te pasa en ingles
+                    haré lo posible por entender y aconsejarte o distraerte`,
         },
         {
             condition: "Ábrela cuando te den ganas de ignorarme (otra vez)",
@@ -54,28 +137,26 @@ function Letter (){
         <div>
             {isTable?
             (<div className={styles.main_container}>
-                {lettersOfLove.map((item, index) => (
-                    <div key={index} className={styles.letterItem}>
-                        <div onClick={() => openPopupLetter(item.content)} className={styles.letterContent}>
-                            <img src="src/assets/letterScreen/letter.png"></img>
+                <div className={styles.letters_column}>
+                    {lettersOfLove.map((item, index) => (
+                        <div key={index} className={styles.letter_item}>
+                            <div onClick={() => openPopupLetter(item.content)} className={styles.letter_content}>
+                                <img src="src/assets/letterScreen/letter.png"></img>
+                            </div>
+                            <div className={styles.letter_condition}>
+                                <div className={styles.letter_circle}/>
+                                <h4>{item.condition}</h4>
+                                <div className={styles.letter_circle}/>
+                            </div>
                         </div>
-                        <h2>{item.condition}</h2>
-                    </div>
-                ))}
-                {popupLetter && (
-                    <div className={styles.popup_overlay} onClick={closePopupLetter}>
-                        <div className={styles.popup_content}>
-                            <h4>{popupLetter}</h4>
-                        </div>
-                        <span className={styles.close_button} onClick={closePopupLetter}></span>
-                    </div>
-                )}
+                    ))}
+                </div>
             </div>)
             :
             (<div className={styles.main_container}>
                 <div className={styles.grid_letters}>
                 {lettersOfLove.map((item, index) => (
-                    <div key={index} className={styles.letterItem}>
+                    <div key={index} className={styles.letter_item}>
                         <div onClick={() => openPopupLetter(item.content)} className={styles.letter_content}>
                             <img src="src/assets/letterScreen/letter.png" className={styles.letter_image}></img>
                         </div>
@@ -86,17 +167,23 @@ function Letter (){
                         </div>
                     </div>
                 ))}
-                {popupLetter && (
-                    <div className={styles.popup_overlay} onClick={closePopupLetter}>
-                        <div className={styles.popup_content}>
-                            <h4>{popupLetter}</h4>
-                        </div>
-                        <span className={styles.close_button} onClick={closePopupLetter}></span>
-                    </div>
-                )}
                 </div>
             </div>)
             }
+            {popupLetter && (
+                    <motion.div className={styles.popup_overlay} onClick={closePopupLetter}>
+                        <motion.div 
+                            className={styles.popup_content} 
+                            onClick={(e) => e.stopPropagation()}
+                            initial={{ scale: 0 }} 
+                            animate={{ scale: 1 }} 
+                            exit={{ scale: 0 }} 
+                            transition={{ duration: 0.3 }}
+                        >
+                            <p>{popupLetter}</p>
+                    </motion.div>
+                </motion.div>
+            )}
         </div>
     );
 }
--- src/screens/NavbarGifts.jsx
+++ src/screens/NavbarGifts.jsx
@@ -1,50 +1,63 @@
 import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router";
 import styles from "../styles/NavbarGifts.module.css";
 
 function NavbarGifts (){
-    const [activeItem, setActiveItem] = useState("puzzle");
+    const [activeItem, setActiveItem] = useState("");
+    const location = useLocation();
 
     const handleItemClick = (item) => {
         setActiveItem(item);
     };
 
+    useEffect(() => {
+        
+        return () => {
+            if(location.hash !== ""){
+                setActiveItem(location.hash);
+            } else if (location.pathname === "/"){
+                setActiveItem("#puzzle");
+            }
+        }
+    }, []);
+
     return (
         <nav className={styles.navigation}>
             <ul>
                 <li
-                    className={`${styles.list} ${activeItem === "puzzle" ? styles.active : ""}`}
-                    onClick={() => handleItemClick("puzzle")}
+                    className={`${styles.list} ${activeItem === "#puzzle" && location.pathname === "/" ? styles.active : ""}`}
+                    onClick={() => handleItemClick("#puzzle")}
                 >
                     <a href="#puzzle">
                         <span className={styles.icon}>
                             <ion-icon name="extension-puzzle"></ion-icon>
                         </span>
                     </a>
-                    {<div className={`${activeItem === "puzzle" ? styles.indicatorActive : styles.indicatorInactive}`}></div>}
+                    {<div className={`${activeItem === "#puzzle" && location.pathname === "/" ? styles.indicatorActive : styles.indicatorInactive}`}></div>}
                 </li>
 
                 <li
-                    className={`${styles.list} ${activeItem === "letter" ? styles.active : ""}`}
-                    onClick={() => handleItemClick("letter")}
+                    className={`${styles.list} ${activeItem === "#letter" && location.hash === "#letter" ? styles.active : ""}`}
+                    onClick={() => handleItemClick("#letter")}
                 >
                     <a href="#letter">
                         <span className={styles.icon}>
                             <ion-icon name="mail"></ion-icon>
                         </span>
                     </a>
-                    {<div className={`${activeItem === "letter" ? styles.indicatorActive : styles.indicatorInactive}`}></div>}
+                    {<div className={`${activeItem === "#letter" && location.hash === "#letter" ? styles.indicatorActive : styles.indicatorInactive}`}></div>}
                 </li>
 
                 <li
-                    className={`${styles.list} ${activeItem === "dice" ? styles.active : ""}`}
-                    onClick={() => handleItemClick("dice")}
+                    className={`${styles.list} ${activeItem === "#dice" && location.hash === "#dice" ? styles.active : ""}`}
+                    onClick={() => handleItemClick("#dice")}
                 >
                     <a href="#dice">
                         <span className={styles.icon}>
                             <ion-icon name="cube"></ion-icon>
                         </span>
                     </a>
-                    {<div className={`${activeItem === "dice" ? styles.indicatorActive : styles.indicatorInactive}`}></div>}
+                    {<div className={`${activeItem === "#dice" && location.hash === "#dice" ? styles.indicatorActive : styles.indicatorInactive}`}></div>}
                 </li>
 
                 {/*<div className={styles.indicator}></div>*/}
